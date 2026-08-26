9a13f740f48b68eef6daf9529b7024ab506e4aa9
Andrey Souza <andreyschowza@gmail.com>
2026-08-26 16:59:18 -0300

    chore: mudar output de dist para docs

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ export default (env, argv) => {
     entry: path.resolve(__dirname, "src", "main.js"),
     output: {
       filename: "main.js",
-      path: path.resolve(__dirname, "dist"),
+      path: path.resolve(__dirname, "docs"),
       clean: true, // Limpa a pasta dist antes de cada build
     },
     
